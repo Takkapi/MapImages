04527f719022697ef46ed39453ebbde16bb7fc9e
Takkapi <72162635+Takkapi@users.noreply.github.com>
2022-01-06 11:27:56 +0200

    Add files via upload

---
+++ src/main/java/md/takkapi/mapImages/models/LogoRenderer.java
@@ -0,0 +1,47 @@
+package md.takkapi.mapImages.models;
+
+import org.bukkit.entity.Player;
+import org.bukkit.map.MapCanvas;
+import org.bukkit.map.MapPalette;
+import org.bukkit.map.MapRenderer;
+import org.bukkit.map.MapView;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+public class LogoRenderer extends MapRenderer {
+    private BufferedImage image;
+    private boolean done;
+
+    public LogoRenderer() {
+        done = false;
+    }
+
+    public LogoRenderer(String url) {
+        load(url);
+        done = false;
+    }
+
+    public boolean load(String url) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new URL(url));
+            image = MapPalette.resizeImage((image)); //less ram usage
+        } catch (IOException e) {
+            return false;
+        }
+        this.image = image;
+        return true;
+    }
+
+    @Override
+    public void render(MapView map, MapCanvas canvas, Player player) {
+        if (done)
+            return;
+
+        canvas.drawImage(0, 0, image);
+        done = true; //super important
+    }
+}
